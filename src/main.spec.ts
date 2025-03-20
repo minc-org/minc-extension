ee1166935b1e6b9ac54b01808f7d20eed00bef64
Florent BENOIT <fbenoit@redhat.com>
2025-03-20 11:41:15 +0100

    chore: please ignore (codecov test)
    Signed-off-by: Florent BENOIT <fbenoit@redhat.com>

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -22,29 +22,3 @@ test('should initialize and activate the MincExtension when activate is called',
   // Ensure that the MincExtension is instantiated and its activate method is called
   expect(MincExtension.prototype.activate).toHaveBeenCalled();
 });
-
-test('should call deactivate when deactivate is called', async () => {
-  // Call activate first to initialize mincExtension
-  await activate(extensionContextMock);
-
-  // Call deactivate
-  await deactivate();
-
-  // Ensure that the deactivate method was called
-  expect(MincExtension.prototype.deactivate).toHaveBeenCalled();
-});
-
-test('should set mincExtension to undefined after deactivate is called', async () => {
-  // Call activate to initialize the extension
-  await activate(extensionContextMock);
-
-  // Call deactivate
-  await deactivate();
-
-  if ('mincExtension' in global) {
-    // Ensure that mincExtension is set to undefined
-    expect(global.mincExtension).toBeUndefined();
-  } else {
-    assert.fail('mincExtension not found in global');
-  }
-});
