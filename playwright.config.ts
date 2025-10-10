21c99b3959fe455a334c6bbc659f146e516b54b2
Daniel Villanueva <davillan@redhat.com>
2025-10-10 12:23:26 +0200

    test(e2e): workflow for e2e tests on linux (#235)
    Signed-off-by: Daniel Villanueva <davillan@redhat.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,18 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testDir: './tests/resources/src',
+  testDir: './tests/playwright/src',
+  outputDir: './tests/playwright/output/',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: 1,
   workers: 1,
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['junit', { outputFile: './tests/playwright/output/junit-results.xml' }],
+    ['json', { outputFile: './tests/playwright/output/json-results.json' }],
+    ['html', { open: 'never', outputFolder: './tests/playwright/output/html-results/' }],
+  ],
   use: {
     trace: 'retain-on-failure',
     video: 'retain-on-failure',
